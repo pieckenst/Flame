7ef5786d0d07eaeb6535c030423ad504c37a007a
torvalds <volodymyr.germany.2020@gmail.com>
2021-03-20 15:45:01 +0100

    Added sharding

---
+++ src/shard.js
@@ -0,0 +1,11 @@
+const { ShardingManager } = require('discord.js');
+const { join } = require('path')
+const config = require('../config.json');
+
+const Manager = new ShardingManager(join(__dirname, 'index.js'), {
+    token: config.token,
+    totalShards: parseInt(config.shards) || 'auto',
+    respawn: true
+});
+
+Manager.spawn(Manager.totalShards, 5500, 400000);
